8aeabf2280fe36d2d6d5b0ef9857cb072bdc4ccc
MarcusJones <jones.0bj3@gmail.com>
2019-01-28 17:24:51 +0100

    add pytest

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -2,7 +2,7 @@ from metadata_validator.json_versions import json4, json1
 from metadata_validator.schema_definitions import valid_schema
 from jsonschema.validators import Draft4Validator
 
-validator = Draft4Validator(valid_schema)
+
 
 
 # %%
@@ -12,6 +12,8 @@ def test_metadata(json_data):
     Test a valid as well as invalid json metadata.
     """
 
+    validator = Draft4Validator(valid_schema)
+    print(validator)
     assert validator.is_valid(json_data)
 
 
