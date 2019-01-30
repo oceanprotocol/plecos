f53f202d5b82fbb91df7da94884c29c0d3666c26
MarcusJones <jones.0bj3@gmail.com>
2019-01-30 11:05:56 +0100

    add Packaging files

---
+++ setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup
+import os
+from os.path import join
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+# Installed by pip install squid-py
+# or pip install -e .
+install_requirements = [
+    jsonschema ="3.0.0b3"
+]
+
+# Required to run setup.py:
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = [
+    'pytest',
+]
+
+# Possibly required by developers of squid-py:
+dev_requirements = [
+    'bumpversion',
+    'pkginfo',
+    'twine',
+    'watchdog',
+]
+
+docs_requirements = [
+    'Sphinx',
+    'sphinxcontrib-apidoc',
+]
+
+# packages = []
+# for d, _, _ in os.walk('squid_py'):
+#     if os.path.exists(join(d, '__init__.py')):
+#         packages.append(d.replace(os.path.sep, '.'))
+
+setup(
+    author="leucothia",
+    author_email='devops@oceanprotocol.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="🐳 Ocean/JSON parsing tool for DDO and metadata.",
+    extras_require={
+        'test': test_requirements,
+        'dev': dev_requirements + test_requirements + docs_requirements,
+        'docs': docs_requirements,
+    },
+    install_requires=install_requirements,
+    license="Apache Software License 2.0",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords='ocean',
+    name='plecos',
+    packages=packages,
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/oceanprotocol/squid-py',
+    version='0.0.01',
+    zip_safe=False,
+)
