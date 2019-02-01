3ce6dc3c445119707fdb8f27e41f080b36ef40c7
MarcusJones <jones.0bj3@gmail.com>
2019-02-01 09:44:31 +0100

    update refactor folders

--- setup.py
+++ setup.py
@@ -22,24 +22,24 @@ install_requirements = [
 ]
 
 # Required to run setup.py:
-setup_requirements = ['pytest-runner', ]
+# setup_requirements = ['pytest-runner', ]
+#
+# test_requirements = [
+#     'pytest',
+# ]
 
-test_requirements = [
-    'pytest',
-]
-
-# Possibly required by developers of squid-py:
-dev_requirements = [
-    'bumpversion',
-    'pkginfo',
-    'twine',
-    'watchdog',
-]
-
-docs_requirements = [
-    'Sphinx',
-    'sphinxcontrib-apidoc',
-]
+# # Possibly required by developers of squid-py:
+# dev_requirements = [
+#     'bumpversion',
+#     'pkginfo',
+#     'twine',
+#     'watchdog',
+# ]
+#
+# docs_requirements = [
+#     'Sphinx',
+#     'sphinxcontrib-apidoc',
+# ]
 
 # data_files = [
 #         ('plecos', glob('schemas/*.json')),
@@ -49,19 +49,19 @@ docs_requirements = [
 # for df in data_files:
 #     print(df)
 
-schema_folder = 'schemas'
-# install_folder = 'lib/python3.6/site-packages/plecos/schemas'
-print("Adding all files in /{}".format(schema_folder))
-data_files = [   (  'plecos'   in   glob(os.path.join(schema_folder, '**/*'))   )  ]
-
-sample_folder = 'samples'
-# install_folder = 'lib/python3.6/site-packages/plecos/samples'
-print("Adding all files in /{}".format(sample_folder))
-data_files.append(   (   'plecos', glob(os.path.join(sample_folder, '**/*'))   ))
-
-print("data_files=")
-for df in data_files:
-    print(df)
+# schema_folder = 'schemas'
+# # install_folder = 'lib/python3.6/site-packages/plecos/schemas'
+# print("Adding all files in /{}".format(schema_folder))
+# data_files = [   (  'plecos'   in   glob(os.path.join(schema_folder, '**/*'))   )  ]
+#
+# sample_folder = 'samples'
+# # install_folder = 'lib/python3.6/site-packages/plecos/samples'
+# print("Adding all files in /{}".format(sample_folder))
+# data_files.append(   (   'plecos', glob(os.path.join(sample_folder, '**/*'))   ))
+#
+# print("data_files=")
+# for df in data_files:
+#     print(df)
 
 
 # packages = []
@@ -81,11 +81,11 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     description="🐳 Ocean/JSON parsing tool for DDO and metadata.",
-    extras_require={
-        'test': test_requirements,
-        'dev': dev_requirements + test_requirements + docs_requirements,
-        'docs': docs_requirements,
-    },
+    # extras_require={
+    #     'test': test_requirements,
+    #     'dev': dev_requirements + test_requirements + docs_requirements,
+    #     'docs': docs_requirements,
+    # },
     install_requires=install_requirements,
     license="Apache Software License 2.0",
     long_description=readme,
@@ -95,13 +95,13 @@ setup(
     name='plecos',
     # packages=packages,
     packages=['plecos'],
-    package_dir={'plecos': 'src/plecos'},
+    # package_dir={'plecos': 'src/plecos'},
     # data_files=data_files,
     # package_data = {'plecos' : files },
-    package_data=data_files,
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
+    # package_data=data_files,
+    # setup_requires=setup_requirements,
+    # test_suite='tests',
+    # tests_require=test_requirements,
     url='https://github.com/oceanprotocol/squid-py',
     version='0.0.2',
     zip_safe=False,
