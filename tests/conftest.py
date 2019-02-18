f22a1fa6d3b88608691c4b96c8fafa070a2789e2
MarcusJones <jones.0bj3@gmail.com>
2019-02-18 10:23:07 +0100

    refactor Changes to OEP8

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,10 @@ import logging
 # Select the latest schema path here
 PATH_SCHEMA_DIR = Path().cwd() / 'plecos' / 'schemas'
 print(PATH_SCHEMA_DIR)
-PATH_LATEST_SCHEMA = PATH_SCHEMA_DIR / 'metadata_v190118.json'
-# PATH_LATEST_SCHEMA = PATH_SCHEMA_DIR / 'supersimple.json'
-# PATH_LATEST_SCHEMA = PATH_SCHEMA_DIR / 'test.json'
+PATH_LATEST_SCHEMA = PATH_SCHEMA_DIR / 'metadata_190218.json'
 assert PATH_LATEST_SCHEMA.exists(), "Path not found: {}".format(PATH_LATEST_SCHEMA)
 
-PATH_SAMPLES_DIR = Path().cwd() / 'samples'
+PATH_SAMPLES_DIR = Path().cwd() / 'plecos' / 'samples'
 PATH_SAMPLE_METADATA = PATH_SAMPLES_DIR / 'metadata sample publisher.json'
 # PATH_SAMPLE_METADATA = PATH_SAMPLES_DIR / 'squid_example.json'
 # PATH_SAMPLE_METADATA = PATH_SAMPLES_DIR / 'supersimple.json'
