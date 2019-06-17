071db4318720ed0bf07670600e4029c049818838
MarcusJones <jones.0bj3@gmail.com>
2019-06-17 12:20:01 +0200

    Bump version: 0.7.5 → 0.7.6

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setuptools.setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/plecos',
-    version='0.7.5',
+    version='0.7.6',
     zip_safe=False,
 )
